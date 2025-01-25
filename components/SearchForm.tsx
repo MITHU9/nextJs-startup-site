ae8dea965374a63a1a5b99cc271638c1837187c5
Shahariar Mithu <kmmithu2015@gmail.com>
2025-01-25 13:11:39 +0600

    add and fetch post

---
+++ components/SearchForm.tsx
@@ -0,0 +1,24 @@
+import Form from "next/form";
+import SearchReset from "./SearchReset";
+import { Search } from "lucide-react";
+
+const SearchForm = ({ query }: { query?: string }) => {
+  return (
+    <Form action="/" scroll={false} className="search-form">
+      <input
+        name="query"
+        defaultValue={query}
+        placeholder="Search for startups"
+        className="search-input"
+      />
+      <div className="flex items-center gap-2">
+        {query && <SearchReset />}
+
+        <button type="submit" className="search-btn text-white">
+          <Search className="size-5" />
+        </button>
+      </div>
+    </Form>
+  );
+};
+export default SearchForm;
